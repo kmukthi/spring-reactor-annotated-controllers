cd0ab72f336e45ab694a3c676b0f40dc0ce69f37
kmukthi <kmukthi@gmail.com>
2021-02-08 14:33:33 +0530

    first commit

---
+++ src/main/java/com/wiredbraincoffee/productapiannotation/ProductApiAnnotationApplication.java
@@ -0,0 +1,36 @@
+package com.wiredbraincoffee.productapiannotation;
+
+import com.wiredbraincoffee.productapiannotation.model.Product;
+import com.wiredbraincoffee.productapiannotation.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class ProductApiAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApiAnnotationApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(ProductRepository productRepository) {
+		return args -> {
+
+			Flux.just(
+					new Product(null, "Big Latte", 2.99),
+					new Product(null, "Big Decaf", 2.49),
+					new Product(null, "Green Tea", 1.99)
+			)
+					.flatMap(p -> productRepository.save(p))
+					.thenMany(productRepository.findAll())
+					.subscribe(System.out::println);
+
+
+
+		};
+	}
+
+}
---
+++ src/main/java/com/wiredbraincoffee/productapiannotation/controller/ProductController.java
@@ -0,0 +1,75 @@
+package com.wiredbraincoffee.productapiannotation.controller;
+
+import com.wiredbraincoffee.productapiannotation.model.Product;
+import com.wiredbraincoffee.productapiannotation.model.ProductEvent;
+import com.wiredbraincoffee.productapiannotation.repository.ProductRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private ProductRepository repository;
+
+    public ProductController(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public Flux<Product> getAllProducts() {
+        return repository.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Mono<ResponseEntity<Product>> getProduct(@PathVariable String id) {
+        return repository.findById(id)
+                .map(p -> ResponseEntity.ok(p))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Product> saveProduct(@RequestBody Product product) {
+        return repository.save(product);
+    }
+
+    @PutMapping("{id}")
+    public Mono<ResponseEntity<Product>> updateProduct(@PathVariable(value = "id") String id,
+                                       @RequestBody Product product) {
+        return repository.findById(id)
+                .flatMap(p -> {
+                    p.setName(product.getName());
+                    p.setPrice(product.getPrice());
+                    return repository.save(p);
+                })
+                .map(p -> ResponseEntity.ok(p))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("{id}")
+    public Mono<ResponseEntity<Void>> deleteProduct(@PathVariable String id) {
+        return repository.findById(id)
+                .flatMap(p -> repository.delete(p).then(Mono.just(ResponseEntity.ok().<Void>build())))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping
+    public Mono<Void> deleteAll() {
+        return repository.deleteAll();
+    }
+
+    @GetMapping(value = "/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<ProductEvent> getProductEvents() {
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(val -> new ProductEvent(val, "Product Event")
+                );
+    }
+
+}
